6129ee4abf14a92a5dd786578f6d470e52146561
Kevinsalinas-07 <00086125@uca.edu.sv>
2026-01-31 14:10:00 -0600

    feat: Insertar al inicio.

--- Labo04.cpp
+++ Labo04.cpp
@@ -20,19 +20,54 @@ bool listaVacia(Nodo *head)
     return head == NULL;
 }
 
-/* Fuicion para buscar paquete por su id */
+/* Funcion para buscar paquete por su id */
 
-bool buscarPorId(Nodo* head, int id) {
-    Nodo* aux = head;
-    while (aux != NULL) {
+bool buscarPorId(Nodo *head, int id)
+{
+    Nodo *aux = head;
+    while (aux != NULL)
+    {
         if (aux->id == id)
             return true;
         aux = aux->sig;
     }
     return false;
-    
 }
+/*
+ Funcion para insertar un paquete al inicio de la lista.
+ Maneja lista vacía y valida ID repetido
+*/
+void insertarInicio(Nodo *&head, Nodo *&tail,
+                    int id, string nombre, float peso)
+{
 
+    // No permitir IDs repetidos
+    if (buscarPorId(head, id))
+    {
+        cout << "ID repetido.\n";
+        return;
+    }
+
+    Nodo *nuevo = new Nodo();
+    nuevo->id = id;
+    nuevo->nombre = nombre;
+    nuevo->peso = peso;
+    nuevo->ant = NULL;
+    nuevo->sig = head;
+
+    // Lista vacía
+    if (listaVacia(head))
+    {
+        head = nuevo;
+        tail = nuevo;
+    }
+    // Lista con elementos
+    else
+    {
+        head->ant = nuevo;
+        head = nuevo;
+    }
+}
 
 int main()
 {
